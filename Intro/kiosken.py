2a103f146c2c212fbd712748bff0441122eb606e
Sillyneer <joab0824@storsjogymnasiet.se>
2025-09-04 14:11:39 +0200

    fint fint

--- Intro/kiosken.py
+++ Intro/kiosken.py
@@ -1,11 +1,11 @@
-print("tjenare välkommen till min kiosk.")
-print("vi har:")
-print("glass för 20kr")
-print("varmkorv för 15kr")
-print("läsk för 15kr")
-print("godis för 10kr")
+print("Tjenare välkommen till min kiosk.")
+print("Vi har:")
+print("Glass för 20kr")
+print("Varmkorv för 15kr")
+print("Läsk för 15kr")
+print("Godis för 10kr")
 
-print("vad skulle du vilja köpa?")
+print("Vad skulle du vilja köpa?")
 
 product = input()
 
@@ -15,32 +15,32 @@ soda_price = 15
 candy_price = 10 
 
 if product == "glass":
-    print("hur många glassar vill du ha")
+    print("Hur många glassar vill du ha")
     amount = input()
     amount = int(amount)
 
-    print("det blir", amount * icecream_price,"kr")
+    print("Det blir", amount * icecream_price,"kr")
 
 elif product == "varmkorv":
-    print("hur många varmkorvar vill du ha")
+    print("Hur många varmkorvar vill du ha")
     amount = input()
     amount = int(amount)
 
-    print("det blir", amount * hotdog_price,"kr")
+    print("Det blir", amount * hotdog_price,"kr")
 
 elif product == "läsk":
-    print("hur mycket läsk vill du ha")
+    print("Hur mycket läsk vill du ha")
     amount = input()
     amount = int(amount)
 
-    print("det blir", amount * soda_price,"kr")
+    print("Det blir", amount * soda_price,"kr")
 
 elif product == "godis":
-    print("hur mycket hg godis vill du ha")
+    print("Hur mycket hg godis vill du ha")
     amount = input()
     amount = int(amount)
 
-    print("det blir", amount * candy_price,"kr")
+    print("Det blir", amount * candy_price,"kr")
 
 
 
